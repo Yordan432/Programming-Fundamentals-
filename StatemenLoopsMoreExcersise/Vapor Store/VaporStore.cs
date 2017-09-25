b0da1557a7233a58dd29a74023b2bf0d453f510e
Yordan432 <dani.583@abv.bg>
2017-09-25 16:19:44 +0300

    Right way

--- StatemenLoopsMoreExcersise/Vapor Store/VaporStore.cs
+++ StatemenLoopsMoreExcersise/Vapor Store/VaporStore.cs
@@ -1,84 +1,102 @@
 ﻿using System;
+using System.Linq;
 
 class VaporStore
 {
     static void Main()
     {
-        double OutFall4 = 39.99;
-        double CSOG = 15.99;
-        double ZplinterZell = 19.99;
-        double Honored2 = 59.99;
-        double RoverWatch = 29.99;
-        double RoverWatchOriginsEdition = 39.99;
-        string empty = String.Empty;
-
-        string OutFall = "OutFall 4";
-        string CSOGString = "CS: OG";
-        string ZplinterZellString = "Zplinter Zell";
-        string Honored2String = "Honored 2";
-        string RoverWatchString = "RoverWatch";
-        string RoverWatchOriginsEditionString = "RoverWatch Origins Edition";
-        
         double balance = double.Parse(Console.ReadLine());
+        string[] games = new string[] { "OutFall 4", "CS: OG", "Zplinter Zell", "Honored 2", "RoverWatch", "RoverWatch Origins Edition" };
+        double[] pricePerGames = { 39.99, 15.99, 19.99, 59.99, 29.99, 39.99 };
 
         string input = Console.ReadLine();
+        double totalSpend = 0;
         while (input != "Game Time")
         {
-            double price = 0;
-            if (input == "OutFall 4")
-            {
-                price = OutFall4;
-                Console.WriteLine("Bought {0}" , OutFall);           
-            }
-            else if(input == "CS: OG")
-            {
-                price = CSOG;
-                Console.WriteLine("Bought {0}", CSOGString);
-
-            }
-            else if (input == "Zplinter Zell")
-            {
-                
-                price = ZplinterZell;
-                Console.WriteLine("Bought {0}", ZplinterZellString);
-
-            }
-            else if (input == "Honored 2")
-            {         
-                price = Honored2;
-                Console.WriteLine("Bought {0}",  Honored2String);
-                
-            }
-            else if (input == "RoverWatch")
-            {
-                price = RoverWatch;
-                Console.WriteLine("Bought {0}",  RoverWatchString);
-
-
-            }
-            else if (input == "RoverWatch Origins Edition")
-            {
-                price = RoverWatchOriginsEdition;
-                Console.WriteLine("Bought {0}",  RoverWatchOriginsEditionString);
-            }
-            else if(input != "RoverWatch Origins Edition" || input != "RoverWatch" || input != "Honored 2" || input != "CS: OG" || input != "OutFall 4" || input != "Zplinter Zell")
+            if (!games.Contains(input))
             {
                 Console.WriteLine("Not Found");
             }
-            balance -= price;
-            if (balance <= price)
+            if (input == "OutFall 4")
             {
-                Console.WriteLine("Too Expensive");
+                if (balance < pricePerGames[0])
+                {
+                    Console.WriteLine("Too Expensive");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                balance -= pricePerGames[0];
+                totalSpend += pricePerGames[0];
+                Console.WriteLine("Bought " + games[0]);
+            }
+            else if (input == "CS: OG")
+            {
+                if (balance < pricePerGames[1] && balance > 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                balance -= pricePerGames[1];
+                totalSpend += pricePerGames[1];
+                Console.WriteLine("Bought " + games[1]);
+            }
+            else if (input == "Zplinter Zell")
+            {
+                if (balance < pricePerGames[2] && balance > 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                balance -= pricePerGames[2];
+                totalSpend += pricePerGames[2];
+                Console.WriteLine("Bought " + games[2]);
+            }
+            else if (input == "Honored 2")
+            {
+                if (balance < pricePerGames[3] && balance > 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                balance -= pricePerGames[3];
+                totalSpend += pricePerGames[3];
+                Console.WriteLine("Bought " + games[3]);
+            }
+            else if (input == "RoverWatch")
+            {
+                if (balance < pricePerGames[4] && balance > 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                balance -= pricePerGames[4];
+                totalSpend += pricePerGames[4];
+                Console.WriteLine("Bought " + games[4]);
+            }
+            else if (input == "RoverWatch Origins Edition")
+            {
+                if (balance < pricePerGames[5] && balance > 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                balance -= pricePerGames[5];
+                totalSpend += pricePerGames[5];
+                Console.WriteLine("Bought " + games[5]);
+            }
+            if (balance <= 0)
+            {
+                Console.WriteLine("Out of money!");
                 return;
             }
 
             input = Console.ReadLine();
-
-            if (balance <= 0)
-            {
-                Console.WriteLine("Out of money!");
-            }
-            
         }
+        Console.WriteLine("Total spent: ${0:f2}. Remaining: ${1:f2}", totalSpend, balance);
     }
 }
