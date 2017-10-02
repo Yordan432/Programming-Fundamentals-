7c84ace264e324b2a3a6cf74c2be98b793264120
Yordan432 <dani.583@abv.bg>
2017-10-02 18:10:44 +0300

    more excercise loops

---
+++ 5.Statemen Loops - More Excersise/X/X.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class X
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int row = 0; row < number; row++)
+        {
+            for (int col = 0; col < number; col++)
+            {
+                if (col == row)
+                {
+                    Console.Write("x");
+                }
+                else if(col == number - row - 1)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
